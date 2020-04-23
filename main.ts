d109c6f212350566d486da5e03de618a84704e29
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-04-24 05:05:24 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * read key
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace keyRead {
+    /**
+     * read key
+     */
+    //% blockId="readKey" block="read key"
+    export function readKey(): string {
+        return "";
+    }
+}
