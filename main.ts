0e7e9f7d38e4fabc04388e03be2799cd59dab9b1
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-04-24 05:26:07 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -8,6 +8,49 @@ namespace keyRead {
      */
     //% blockId="readKey" block="read key"
     export function readKey(): string {
-        return "";
+        const timeLimit = 5000;
+        const longPress = 500;
+        const clickInterval = 500;
+
+        let startTime = input.runningTimeMicros() / 1000
+        let lastKeyTime = 0;
+        let keyData = "";
+        let tempKey = "";
+
+        while (input.runningTimeMicros() / 1000 < startTime + timeLimit && keyData == "") {
+            if (input.buttonIsPressed(Button.AB)) {
+                if (tempKey != "AB") {
+                    lastKeyTime = input.runningTimeMicros() / 1000;
+                }
+                tempKey = "AB";
+            } else if (input.buttonIsPressed(Button.A)) {
+                if (tempKey != "A") {
+                    lastKeyTime = input.runningTimeMicros() / 1000;
+                }
+                tempKey = "A";
+            } else if (input.buttonIsPressed(Button.B)) {
+                if (tempKey != "B") {
+                    lastKeyTime = input.runningTimeMicros() / 1000;
+                }
+                tempKey = "B";
+            } else if (input.isGesture(Gesture.Shake)) {
+                if (tempKey != "S") {
+                    lastKeyTime = input.runningTimeMicros() / 1000;
+                }
+                tempKey = "S";
+            } else {
+                if (tempKey != "") {
+                    if (input.runningTimeMicros() / 1000 > lastKeyTime + longPress) {
+                        keyData = "L-" + tempKey;
+                    } else {
+                        keyData = tempKey;
+                    }
+                }
+            }
+        }
+        if (input.runningTimeMicros() / 1000 >= startTime + timeLimit) {
+            return "";
+        }
+        return keyData;
     }
 }
